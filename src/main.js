9de52190b51f21680c6fc71ce6f4cf5aa98f6a02
mydylan <mydylan7@gmail.com>
2016-06-04 18:19:39 +0300

    building header

--- src/main.js
+++ src/main.js
@@ -5,3 +5,8 @@ import Style from './stylesheets/app.sass';
 
 const app = new App(DATA);
 app.render();
+
+document.querySelector('.filter-button').addEventListener("click", () => {
+  document.querySelector('.filters').classList.toggle('open');
+  document.querySelector('.caret').classList.toggle('rotate');
+});
